831f3a71cc2fd867175b955c100bc9fb8893250b
Dima-Bobeico <66416198+Dima-Bobeico@users.noreply.github.com>
2021-02-22 17:48:40 +0200

    a

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return(
+        <div>
+            <ul>
+                <li className="active"><a href="#">Dima Bobeico</a></li>
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="far fa-envelope"></i></a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
